a73956a1762f004aadb83edcffa24295a05aeda5
kjm <willom2c@naver.com>
2024-01-24 09:33:26 +0900

    [feat]: EC2에 있는 MySQL 연결 성공, 폴더구조 대충 잡음

---
+++ BACKEND/fodong/src/main/java/com/adhd/fodong/FodongApplication.java
@@ -0,0 +1,13 @@
+package com.adhd.fodong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FodongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FodongApplication.class, args);
+	}
+
+}
---
+++ BACKEND/fodong/src/test/java/com/adhd/fodong/FodongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adhd.fodong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FodongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
