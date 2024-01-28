480485d566ecc433bb909c3084e144b509a74bac
전재완 <wodhksdl7893@naver.com>
2024-01-28 17:28:45 +0900

    [feat] : 동화구연 페이지 넘기기 기능

--- FRONT/fodong/src/components/storyTelling/Page1.jsx
+++ FRONT/fodong/src/components/storyTelling/Page1.jsx
@@ -1,25 +1,51 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 // import MainBook from "./img/Mainbook.png";
 import "./StoryTelling.css";
+import DummyScript from "./DummyScript";
+
 
 const Page = () => {
   const [imageSrc, setImageSrc] = useState('');
-  const  { page }  = useParams();
-
+  const  { page : pageParam }  = useParams();
+  const [page, setPage] = useState(parseInt(pageParam, 10) || 1);
+  const navigate = useNavigate();
+  
   useEffect(() => {
+    const imgeUrl = require(`./img/background${pageParam}.png`)
     // fetch(`http://.com/api/image?page=${page}`)
       // .then((response) => response.json())
       // .then((data) => setImageSrc(data.imageUrl));
-      const imgeUrl = require(`./img/background${page}.png`)
+
+      // const imgeUrl = require(`./img/background${page}.png`)
       setImageSrc(imgeUrl);
-      // const imageModule = require(`./img/background${page}.png`)
-      // setImageSrc(images(imageModule));
-  }, [page]);
+
+  }, [page, pageParam]);
+
+  const handleNextPage = () => {
+    const nextPage = page + 1;
+    console.log(11, page)
+    if ( nextPage <= DummyScript.length ) {
+      navigate(`/storytelling/${nextPage}`);
+      setPage(nextPage);
+    }
+  //    else if (page < DummyScript.length) {
+  //     const nextpage = page + 1;
+  //     navigate(`/storytelling/${nextpage}`);
+  //     setPage(nextpage);
+  //     setScriptIndex(0);
+  //     setScript(DummyScript[nextpage -1][0].text);
+  }
+  // ;}
 
   return (
+    <div>
+      <button variant="filled" onClick={handleNextPage}>
+        {'>'}
+      </button>
     <div className="story_img">
       <img src={imageSrc} alt={`page ${page}`} style={{width : '50vw', height: '50vh'}}/>
+    </div>
     </div>
   );
 };
--- FRONT/fodong/src/components/storyTelling/Script.jsx
+++ FRONT/fodong/src/components/storyTelling/Script.jsx
@@ -4,35 +4,56 @@ import "./StoryTelling.css";
 import DummyScript from "./DummyScript";
 import { useParams, useNavigate } from "react-router-dom";
 
-
 const Script = () => {
-  const { page : pageParam} = useParams();
+  const { page: pageParam } = useParams();
   const [page, setPage] = useState(parseInt(pageParam, 10) || 1);
   // const [page, setPage] = useState(useParams().page);
   const [scriptIndex, setScriptIndex] = useState(0);
-  console.log(page);
+  // console.log(page);
   const [script, setScript] = useState(DummyScript[page - 1][scriptIndex].text);
   const navigate = useNavigate();
 
+  const [prevPage, setPrevPage] = useState(page)
+
   useEffect(() => {
-    setScript(DummyScript[page - 1][scriptIndex].text);
-  }, [scriptIndex, page]);
+    //   setScript(DummyScript[page - 1][scriptIndex].text);
+    // }, [scriptIndex, page]);
+
+    const numPage = parseInt(pageParam, 10);
+    console.log(numPage);
+    if (numPage !== page) {
+      setPage(numPage);
+    
+      if (numPage !== prevPage) {
+        setScriptIndex(0);
+        // setScript(DummyScript[page - 1][0].text);
+        setPrevPage(numPage);
+      }
+    }
+      if (DummyScript[numPage - 1] && DummyScript[numPage - 1][scriptIndex]) {
+        setScript(DummyScript[numPage - 1][scriptIndex].text);
+    }
+  }, [pageParam, page, scriptIndex, prevPage]);
 
   // idx+1 < maxlength-1 ? idx+1 : nextpage
   const handleNextScript = () => {
     const scriptLength = DummyScript[page - 1].length;
 
     if (scriptIndex + 1 < scriptLength) {
-      setScriptIndex((idx) => idx + 1);}
-     else if (page < DummyScript.length) {
-      const nextpage = page + 1;
-      navigate(`/storytelling/${nextpage}`);
-      setPage(nextpage);
+      setScriptIndex((idx) => idx + 1);
+    } else if (page < DummyScript.length) {
+      console.log(scriptIndex)
+      console.log(page )
+      console.log(setPage)
+      const nextPage = page + 1;
+      navigate(`/storytelling/${nextPage}`);
+      setPage(nextPage);
       setScriptIndex(0);
-      setScript(DummyScript[nextpage -1][0].text);
-  };}
+      setScript(DummyScript[nextPage - 1][0].text);
+    }
+  };
 
-  console.log(script);
+  // console.log(script);
   return (
     <div className="main-content">
       <div className="script-container">
@@ -48,5 +69,5 @@ const Script = () => {
     </div>
   );
 };
-  
+
 export default Script;
