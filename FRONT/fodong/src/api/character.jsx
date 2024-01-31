b9204ebce705d97ed3d47e99b483d233cb9f183c
전재완 <wodhksdl7893@naver.com>
2024-01-31 16:44:42 +0900

    [feat] : 캐릭터 선택창 백에서 받아오기 위하여 코드 수정

---
+++ FRONT/fodong/src/api/character.jsx
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+export const character = async(imageUrl) => {
+    try {
+        const response = await axios.get(imageUrl);
+        return response.data;
+    } catch (e) {
+        throw e;
+    }
+
+  
+}
