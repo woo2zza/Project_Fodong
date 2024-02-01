b959b7578b84b3d4a401b4fd8bf445965cecf883
kjm <willom2c@naver.com>
2024-02-01 17:56:19 +0900

    [feat] : webRTC 세션설정중
    Former-commit-id: 2f64c6fa096e8327bf84eb851296ca526ba17fd7

--- BACKEND/fodong/src/main/java/com/adhd/fodong/domain/gameroom/webrtc/controller/WebRtcController.java
+++ BACKEND/fodong/src/main/java/com/adhd/fodong/domain/gameroom/webrtc/controller/WebRtcController.java
@@ -1,6 +1,7 @@
 package com.adhd.fodong.domain.gameroom.webrtc.controller;
 
 import io.openvidu.java.client.*;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,20 +22,25 @@ public class WebRtcController {
 
     private OpenVidu openvidu;
 
+    @PostConstruct
     public void init() {
         this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+        System.out.println("openvidu 객체 생성 : " + openvidu);
     }
 
     @PostMapping("/api/sessions")
     public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params)
             throws OpenViduJavaClientException, OpenViduHttpException {
+        System.out.println("---------------신규 세션 생성-----------------");
 
         // 설정한 세션
         SessionProperties properties = SessionProperties.fromJson(params).build();
-
+        System.out.println("세션 설정 정보 : " + properties);
 
         // 활성화된 세션 목록에 만든 세션 id를 올림
         Session session = openvidu.createSession(properties);
+        System.out.println("만든 세션 정보 : " + session);
+        System.out.println("---------------------------------------------");
         return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
     }
 
