865bbf31b13f3e039b232c250a4492e50496a4a5
전재완 <wodhksdl7893@naver.com>
2024-02-02 16:18:51 +0900

    [feat] : JS코드로 웹캠에서 얼굴만 추출하여 띄우기
    Former-commit-id: 7ac3be3ea6b120bd46dc6659093fd2a28fd2c093

--- FRONT/fodong/src/components/face/Face.jsx
+++ FRONT/fodong/src/components/face/Face.jsx
@@ -1,73 +1,104 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import * as faceapi from "face-api.js";
-import jjwImage from "./img/ljh.jpg";
+import * as hull from "hull.js";
 import grass from "./img/베짱이1.png";
 
 function App() {
-  const [modelsLoaded, setModelsLoaded] = React.useState(false);  // 모델 로딩 상태를 관리하는 state를 정의합니다. 초기값은 false입니다.
-  const [facePosition, setFacePosition] = React.useState({ x: 250, y: 550 }); // 얼굴을 그릴 위치
-  const [faceSize, setFaceSize] = React.useState({ width: 200, height: 200 }); // 얼굴을 그릴 크기
-  const canvasRef = React.useRef(); // canvas에 대한 참조를 생성합니다.
-  const imageRef = React.useRef(); // 이미지에 대한 참조를 생성합니다.
+  // React 컴포넌트 정의
+  const [modelsLoaded, setModelsLoaded] = useState(false); // 모델 로드 상태를 관리하는 상태 변수
+  const videoRef = useRef(); // 웹캠 비디오 요소를 참조하는 useRef
+  const canvasRef = useRef(); // 캔버스 요소를 참조하는 useRef
 
-  React.useEffect(() => { // 컴포넌트가 마운트되거나, 지정된 의존성이 변경될 때 실행될 효과를 정의합니다.
-    const loadModelsAndDetectFaces = async () => { // 모델을 로드하고 얼굴을 감지하는 비동기 함수를 정의합니다.
-      const MODEL_URL = process.env.PUBLIC_URL + "/models"; // 모델 URL을 정의합니다.
-
-      await Promise.all([ // 모든 모델이 로드될 때까지 기다립니다.
-        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
-        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
-        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
-        faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
-      ]);
-      setModelsLoaded(true); // 모델 로딩 상태를 true로 업데이트합니다.
-
-      const imgEl = imageRef.current; // 이미지 참조에서 현재 값을 가져옵니다
-      const canvas = canvasRef.current; // canvas 참조에서 현재 값을 가져옵니다.
-      if (canvas && imgEl) { // canvas와 imgEl이 모두 존재하는지 확인
-        const ctx = canvas.getContext("2d"); // 2D rendering context를 가져옵니다.
-        if (ctx) { // ctx가 정의되어 있는지 확인
-          const detections = await faceapi
-            .detectAllFaces(imgEl, new faceapi.TinyFaceDetectorOptions()) // 이미지에서 모든 얼굴을 감지합니다.
-            .withFaceLandmarks(); // 얼굴 랜드마크와 함께 감지합니다.
-          const resizedDetections = faceapi.resizeResults(detections, {  // 감지된 결과를 이미지 크기에 맞게 조정합니다.
-            width: imgEl.width,
-            height: imgEl.height,
-          });
-          canvas.width = imgEl.width; // canvas의 너비를 이미지의 너비로 설정합니다.
-          canvas.height = imgEl.height; // canvas의 높이를 이미지의 높이로 설정합니다.
-
-          ctx.clearRect(0, 0, canvas.width, canvas.height); // canvas를 클리어합니다.
-
-          resizedDetections.forEach((detection) => { // 감지된 얼굴마다 반복합니다.
-            const { x, y, width, height } = detection.detection.box; // 감지된 얼굴의 bounding box 정보를 가져옵니다.
-            const centerX = facePosition.x;  // 얼굴을 그릴 중심 x 좌표입니다.
-            const centerY = facePosition.y; // 얼굴을 그릴 중심 y 좌표입니다.
-            const radius = Math.max(faceSize.width, faceSize.height) / 2 ; // 얼굴을 그릴 반지름을 계산합니다
-
-            ctx.beginPath(); // 새로운 경로를 시작합니다.
-            ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false); // 얼굴을 그릴 원을 그립니다.
-            ctx.clip(); // 경로를 클리핑합니다.
-
-            ctx.drawImage( // 이미지를 canvas에 그립니다.
-              imgEl,
-              x,
-              y,
-              width,
-              height,
-              facePosition.x - radius,
-              facePosition.y - radius,
-              faceSize.width,
-              faceSize.height
-            );
-
-            ctx.restore(); // 이전 상태로 복원합니다.
-          });
-        }
-      }
+  // 모델을 로드하는 함수
+  useEffect(() => {
+    const loadModels = async () => {
+      const MODEL_URL = process.env.PUBLIC_URL + "/models";
+      await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceLandmark68TinyNet.loadFromUri(MODEL_URL);
+      setModelsLoaded(true); // 모델 로딩이 완료되면 모델 로드 상태를 true로 설정
     };
-    loadModelsAndDetectFaces();
-  }, [facePosition, faceSize]); // 위치와 크기가 변경되면 다시 감지하고 그립니다.
+    loadModels(); // loadModels 함수 호출
+  }, []); // 컴포넌트가 마운트될 때 한 번만 실행
+
+  // 웹캠을 설정하는 함수
+  useEffect(() => {
+    if (modelsLoaded) {
+      // 모델이 로딩되었을 때만 실행
+      const video = videoRef.current; // 웹캠 비디오 요소 참조
+      navigator.getUserMedia(
+        { video: {} },
+        (stream) => {
+          video.srcObject = stream; // 웹캠 스트림을 비디오 요소에 할당하여 웹캠 화면 표시
+        },
+        (err) => console.error(err)
+      );
+      video.play().then(() => {
+        detectFace(); // 웹캠 비디오 재생 시작 후 얼굴 감지 함수 호출
+      });
+    }
+  }, [modelsLoaded]); // modelsLoaded 상태가 변경될 때마다 실행
+
+  // 얼굴을 감지하고 윤곽선을 그리는 함수
+  const detectFace = async () => {
+    const video = videoRef.current;
+    const canvas = canvasRef.current;
+    const displaySize = { width: video.width, height: video.height };
+
+    faceapi.matchDimensions(canvas, displaySize); // 캔버스 크기를 비디오 크기와 일치시킴
+
+    setInterval(async () => {
+      // 일정 시간 간격으로 얼굴 감지 실행
+      const detections = await faceapi
+        .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()) // 얼굴 탐지 실행
+        .withFaceLandmarks(true); // 얼굴 랜드마크도 함께 감지
+
+      const resizedDetections = faceapi.resizeResults(detections, displaySize); // 감지 결과 크기 조정
+      const ctx = canvas.getContext("2d"); // 캔버스 2D 그래픽 컨텍스트 얻기
+      canvas.width = video.width;
+      canvas.height = video.height;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      resizedDetections.forEach((detection) => {
+        // 각 얼굴에 대해 반복
+        if (detection && detection.detection) {
+          ctx.save();
+          cropContour(ctx, [
+            ...detection.landmarks.getJawOutline(),
+            ...detection.landmarks.getLeftEyeBrow(),
+            ...detection.landmarks.getRightEyeBrow(),
+          ]); // 얼굴 윤곽을 찾아내고 클리핑
+          ctx.clip();
+          ctx.drawImage(video, 0, 0, video.width, video.height);
+          ctx.restore();
+        }
+      });
+    }, 100); // 100ms마다 감지를 수행합니다.
+  };
+
+  // hull.js를 사용하여 얼굴 주변의 윤곽선을 찾아내고 캔버스에 그리는 함수
+  const cropContour = (ctx, points, isClosed = false) => {
+    const hullPoints = hull(points, 300, [".x", ".y"]);
+    ctx.beginPath();
+    hullPoints.slice(1).forEach(({ x, y }) => {
+      // 윤곽선을 찾아내는 hull 함수 호출
+      ctx.lineTo(x, y); // 윤곽선을 그리기 위해 점들을 연결
+    });
+
+    if (isClosed) {
+      // 윤곽선이 닫혀 있는 경우
+      const from = hullPoints[hullPoints.length - 1];
+      const to = hullPoints[0];
+      if (from && to) {
+        ctx.moveTo(from.x, from.y);
+        ctx.lineTo(to.x, to.y); // 처음과 끝을 연결하여 윤곽선을 닫음
+      }
+    }
+
+    ctx.closePath();
+    ctx.stroke();
+    ctx.clip();
+  };
+
   return (
     <div
       style={{
@@ -77,23 +108,15 @@ function App() {
         backgroundSize: "cover",
       }}
     >
-      {modelsLoaded ? ( // 모델이 로드되었는지에 따라 조건부 렌더링을 수행합니다.
-        <div style={{ position: "relative" }}>
-          <img
-            ref={imageRef}
-            src={jjwImage}
-            alt="Source"
-            style={{ display: "none" }}
-          />
-          <canvas
-            ref={canvasRef}
-            style={{ position: "absolute", left: 100, top: 0 }}
-          />
-          {/* 여기에 위치와 크기를 조절할 수 있는 인터페이스를 추가할 수 있습니다. */}
-        </div>
-      ) : (
-        <div>Loading models...</div>
-      )}
+      <video
+        ref={videoRef}
+        width="1366"
+        height="1024"
+        autoPlay
+        muted
+        style={{ display: "none" }}
+      ></video>
+      <canvas ref={canvasRef} width="1366" height="1024" />
     </div>
   );
 }
