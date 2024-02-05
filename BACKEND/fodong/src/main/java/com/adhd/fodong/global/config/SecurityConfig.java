031ab51a208972c26e27413eb9ebb016f83d8d46
kjm <willom2c@naver.com>
2024-02-05 13:44:19 +0900

    [ETC] : docker test
    Former-commit-id: 372a25d92f0293c4d8a43b672853bb10e2da607b
    Former-commit-id: 56bc6e1a19a9a557ca82d87fb84af3a5f2ab475d

--- BACKEND/fodong/src/main/java/com/adhd/fodong/global/config/SecurityConfig.java
+++ BACKEND/fodong/src/main/java/com/adhd/fodong/global/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
                                 "http://192.168.100.159:3000",
                                 "http://192.168.100.158:3000",
                                 "https://i10c109.p.ssafy.io",
+                                "https://i10c109.p.ssafy.io:3000",
                                 "http://192.168.0.12:3000",
                                 "http://172.30.1.66:3000"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
