fdf0871a138a3418d8ef5b5f9221ec3ff78cc497
심우석 <dntjr5741@naver.com>
2024-02-08 14:16:10 +0900

    [feat] : main 더미데이터 백엔드와 연결
    Former-commit-id: d0b2a7e6cd09b2024ccdd8b440d58185ac08582c
    Former-commit-id: 13af3f4dcbdcdf52ae08bb590b15fba6fbdab88b

--- FRONT/fodong/src/components/main/scrollBook.jsx
+++ FRONT/fodong/src/components/main/scrollBook.jsx
@@ -1,78 +1,57 @@
 import React, { useState, useEffect } from "react";
 import CharModal from "./CharModal";
 import "./mainStyle.css";
-import Image1 from "./dummy_cover/2.jpg";
-import Image2 from "./dummy_cover/3.jpg";
-import Image3 from "./dummy_cover/4.jpg";
-import Image4 from "./dummy_cover/5.jpg";
-import Image5 from "./dummy_cover/6.jpg";
-import Image6 from "./dummy_cover/7.jpg";
+import axios from "axios";
 
-const books = [
-  {
-    id: 1,
-    image: Image1,
-    title: "토끼와 호랑이",
-    characters: "토끼, 호랑이",
-    description: "호랑이와 토끼의 이야기",
-  },
-  {
-    id: 2,
-    image: Image2,
-    title: "개구리",
-    characters: "개구리",
-    description: "개구리의 이야기",
-  },
-  {
-    id: 3,
-    image: Image3,
-    title: "선녀와 나무꾼",
-    characters: "선녀, 나무꾼",
-    description: "선녀와 나무꾼의 이야기",
-  },
-  {
-    id: 4,
-    image: Image4,
-    title: "호랑이와 감",
-    characters: "감, 호랑이",
-    description: "호랑이와 감의 이야기",
-  },
-  {
-    id: 5,
-    image: Image5,
-    title: "견우와 직녀",
-    characters: "견우, 직녀, 까마귀",
-    description: "견우와 직녀의 이야기",
-  },
-  {
-    id: 6,
-    image: Image6,
-    title: "이상한 나라의 앨리스",
-    characters: "앨리스",
-    description: "이상한 나라의 앨리스의 이야기",
-  },
-];
-function ScrollBook() {
+const API_URL = process.env.REACT_APP_API_URL;
+const API_BASE_URL = `${API_URL}/books`;
+
+const ScrollBook = () => {
+  const [books, setBooks] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태
   const [selectedBook, setSelectedBook] = useState({}); // 선택된 동화책
+
+  const accountId = localStorage.getItem("accountId");
+  const token = localStorage.getItem("Token");
+  const config = {
+    headers: {
+      Authorization: `${token}`,
+    },
+  };
+
+  useEffect(() => {
+    if (!accountId) {
+      console.log("No accountId available");
+      return;
+    }
+
+    axios
+      .get(`${API_BASE_URL}`, config)
+      .then((response) => {
+        setBooks(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("서버 요청 실패:", error);
+      });
+  }, [accountId, token]);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % books.length);
     }, 4000);
     return () => clearTimeout(timer);
-  }, [currentIndex]);
+  }, [currentIndex, books.length]);
 
   const openModal = (bookId) => {
-    console.log(bookId); // 로그 출력
     const bookData = books.find((book) => book.id === bookId);
     if (bookData) {
       setSelectedBook(bookData);
       setIsModalOpen(true);
     }
   };
-  // ...
+
   const goToNextBook = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % books.length);
   };
@@ -95,7 +74,7 @@ function ScrollBook() {
           <div
             key={index}
             className={`slide ${index === currentIndex ? "active" : ""}`}
-            style={{ backgroundImage: `url(${book.image})` }}
+            style={{ backgroundImage: `url(${book.cover})` }}
             onClick={() => openModal(book.id)}
           >
             <div className="book-info">
@@ -116,6 +95,6 @@ function ScrollBook() {
       </button>
     </div>
   );
-}
+};
 
 export default ScrollBook;
--- FRONT/fodong/src/components/readBook/Book.jsx
+++ FRONT/fodong/src/components/readBook/Book.jsx
@@ -5,18 +5,16 @@ import "./book.css";
 // A 컴포넌트 정의
 const A = () => {
   // 총 페이지 수와 FlipPage 컴포넌트를 조작하기 위한 ref
-  const totalPage = 15;
+
   const flipBookRef = useRef(null);
-  const Image1 = `${process.env.PUBLIC_URL}/img/dummy_cover/2.jpg`;
-  const Image2 = `${process.env.PUBLIC_URL}/img/dummy_cover/3.jpg`;
-  const Image3 = `${process.env.PUBLIC_URL}/img/dummy_cover/4.jpg`;
-  const Image4 = `${process.env.PUBLIC_URL}/img/dummy_cover/5.jpg`;
-  const Image5 = `${process.env.PUBLIC_URL}/img/dummy_cover/6.jpg`;
-  const Image6 = `${process.env.PUBLIC_URL}/img/dummy_cover/7.jpg`;
-  const Image7 = `${process.env.PUBLIC_URL}/img/dummy_cover/8.jpg`;
-  const Image8 = `${process.env.PUBLIC_URL}/img/dummy_cover/9.jpg`;
-  const Image9 = `${process.env.PUBLIC_URL}/img/dummy_cover/10.jpg`;
-  const Image10 = `${process.env.PUBLIC_URL}/img/dummy_cover/1.jpg`;
+  const Image1 = `${process.env.PUBLIC_URL}/img/pinokio/1.webp`;
+  const Image2 = `${process.env.PUBLIC_URL}/img/pinokio/2.webp`;
+  const Image3 = `${process.env.PUBLIC_URL}/img/pinokio/3.webp`;
+  const Image4 = `${process.env.PUBLIC_URL}/img/pinokio/4.webp`;
+  const Image5 = `${process.env.PUBLIC_URL}/img/pinokio/5.webp`;
+  const Image6 = `${process.env.PUBLIC_URL}/img/pinokio/6.webp`;
+  const Image7 = `${process.env.PUBLIC_URL}/img/pinokio/7.webp`;
+  const Image8 = `${process.env.PUBLIC_URL}/img/pinokio/8.webp`;
   const pageImages = [
     Image1,
     Image2,
@@ -26,16 +24,25 @@ const A = () => {
     Image6,
     Image7,
     Image8,
-    Image9,
-    Image10,
   ];
+  const Script = [
+    "제페토 할아버지가 마법의 나무로 인형을 만들었어요. 그 이름은 피노키오! 제페토 할아버지는 피노키오가 진짜 소년이 되기를 바랐죠.",
+    "피노키오가 갑자기 움직이기 시작했어요. '나 살아있어!' 제페토 할아버지는 피노키오를 사랑으로 가르치기 시작했답니다.",
+    "제페토 할아버지는 피노키오를 학교에 보냈어요. 하지만 피노키오는 인형극단에 끌려가고 말았죠.",
+    "인형극단 주인의 속임수에 빠진 피노키오. 하지만 친절한 요정의 도움으로 위험에서 벗어났어요.",
+    "피노키오는 제페토 할아버지를 찾다가 바다에 빠져 거대한 고래에게 삼켜졌어요. 고래 배 속에서 제페토 할아버지를 만났답니다.",
+    "피노키오와 제페토 할아버지는 고래 배 속에서 서로를 꼭 안았어요. 그들은 함께 탈출 계획을 세웠죠.",
+    "집으로 돌아온 피노키오는 학교에 다니며 성실하게 살기 시작했어요. 요정의 말대로 좋은 행동을 하기로 했죠.",
+    "피노키오의 선한 마음은 결국 마법으로 보상받았어요. 아침에 일어나 보니 피노키오는 진짜 소년이 되었답니다! 제페토 할아버지와 피노키오는 행복하게 살았어요.",
+  ];
+  const numberOfImages = pageImages.length;
+  const totalPage = numberOfImages + 1;
   // 표지 페이지를 위한 컴포넌트
   // React.forwardRef를 사용해 ref를 내부 div로 전달
   const PageCover = React.forwardRef(({ title }, ref) => (
     <div className="page page-cover" ref={ref}>
-      <div className="page-content">
-        <h2>{title}</h2>
-      </div>
+      <h2>{title}</h2>
+      <div className="page-content"></div>
     </div>
   ));
 
@@ -73,11 +80,12 @@ const A = () => {
           onPageClick={handlePageClick}
           ref={flipBookRef}
         >
-          <PageCover title="BOOK TITLE" />
+          <PageCover title="피 노 키 오" />
           {/* 중간 페이지들을 렌더링합니다. */}
           {Array.from({ length: totalPage - 2 }).map((_, index) => (
             <div key={index} className="page-content">
               {/* 페이지에 이미지 추가 */}
+
               <div
                 className="page-image"
                 style={{
@@ -87,6 +95,7 @@ const A = () => {
                   backgroundSize: "cover",
                 }}
               ></div>
+              <div className="script-text">{Script[index]}</div>
             </div>
           ))}
           <PageCover title="THE END" />
