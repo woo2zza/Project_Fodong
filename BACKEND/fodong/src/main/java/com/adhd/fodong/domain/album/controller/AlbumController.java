cc9d332dfc2f207b044335dc88c1634af9e64c25
김지현 <kjihyun0331@naver.com>
2024-02-08 14:38:56 +0900

    [feat] Developing Album Controller - Recording Create
    Former-commit-id: 7c7d156856c9a558ba62c3fd4a1e82c37dbcc3e5
    Former-commit-id: b664ff2674e6f628cba3558a0bdbc0e1cc09f366

--- BACKEND/fodong/src/main/java/com/adhd/fodong/domain/album/controller/AlbumController.java
+++ BACKEND/fodong/src/main/java/com/adhd/fodong/domain/album/controller/AlbumController.java
@@ -5,9 +5,11 @@ import com.adhd.fodong.domain.album.service.AlbumService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+/*
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/album")
@@ -15,10 +17,20 @@ public class AlbumController {
 
     private final AlbumService albumService;
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/save")
+    public String uploadRecording(
+            @RequestParam("video") MultipartFile video,
+            @RequestParam("profile_id") int profileId,
+            @RequestParam("book_id") int bookId
+    ){
+
+    }
+
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{profileId}")
     public List<RecordingEntity> getRecordings(@PathVariable int profileId) {
         List<RecordingEntity> recordings = albumService.getRecordings(profileId);
         return recordings;
     }
-}
+}*/
