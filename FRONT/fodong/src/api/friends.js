46c8566b0a07b997ba825bd7cbfebdc005fc8545
GureumKim <chh5680@naver.com>
2024-02-10 15:06:47 +0900

    [feat] : 친구 요청 보내는 건 ok - 알림 보내기 진행 중
    Former-commit-id: 520cc93b1d0bb004d0f8c18ec18688c6f1bb3a5a
    Former-commit-id: b2accc076e0cbf516959a175a150dd92232f1111

--- FRONT/fodong/src/api/friends.js
+++ FRONT/fodong/src/api/friends.js
@@ -7,14 +7,42 @@ const searchNickname = async (nickname, token) => {
   const params = { nickname };
 
   const response = await axios
-    .get(`${API_URL}/friends/search`, params, {
+    .get(`${API_URL}/friends/search`, {
+      params,
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `${token}`,
       },
     })
-    .then((response) => response.data)
-    .catch((error) => console.error(error));
-  // return response.data;
+    .then((response) => {
+      // console.log(response.data);
+      return response.data;
+    })
+    .catch((error) => {
+      console.error(error);
+      return [];
+    });
+  return response;
 };
 
-export { searchNickname };
+const addFriends = async (fromProfileId, toProfileId, token) => {
+  const data = { fromProfileId, toProfileId };
+  const response = await axios
+    .post(`${API_URL}/friends/requests`, data, {
+      headers: {
+        Authorization: `${token}`,
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+      return response.data;
+    })
+    .catch((error) => {
+      console.error(error);
+      return "친구 요청 에러:" + error;
+    });
+
+  return response;
+};
+
+export { searchNickname, addFriends };
