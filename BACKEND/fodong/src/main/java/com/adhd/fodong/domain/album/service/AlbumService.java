56ab0ad7e4ab42e1a7f79ec9beaaa3a7302f2d7c
김지현 <kjihyun0331@naver.com>
2024-02-11 18:07:08 +0900

    [feat] Update Album Feature
    Former-commit-id: b493737b6c7631113c8bca441b5734aee3e66baf
    Former-commit-id: 48422411d8e8b3350643f831886d4df6de823662

--- BACKEND/fodong/src/main/java/com/adhd/fodong/domain/album/service/AlbumService.java
+++ BACKEND/fodong/src/main/java/com/adhd/fodong/domain/album/service/AlbumService.java
@@ -2,10 +2,11 @@ package com.adhd.fodong.domain.album.service;
 
 import com.adhd.fodong.domain.album.dto.RecordingDetails;
 import com.adhd.fodong.domain.album.dto.RecordingDto;
-import com.adhd.fodong.domain.album.entity.RecordingEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface AlbumService {
     public void save(RecordingDto recordingDto);
     public List<RecordingDetails> getRecordings(int profileId);
