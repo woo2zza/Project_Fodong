64ff8fbb6aa7cfdce2b25555f58e642e36cd56d7
심우석 <dntjr5741@naver.com>
2024-02-11 16:35:10 +0900

    [feat] : CharModal detail
    Former-commit-id: 28f85ee334853be058daa6bd0ea9480649727a1d
    Former-commit-id: 6b331bc10cfaa5f1f18708a1de5e48d1623e9995

--- FRONT/fodong/src/components/main/Title.jsx
+++ FRONT/fodong/src/components/main/Title.jsx
@@ -1,5 +1,13 @@
-const Title = ({ profileId }) => {
-  return <div className="titleStyle">Welcome to Kid's World! {profileId}</div>;
+import { userStore } from "../../store/userStore";
+// import { Logo } from "./image/logo.png";
+const Title = ({ nickname }) => {
+  const useNickname = userStore((state) => state.nickname);
+  return (
+    <div className="titleStyle">
+      {/* <img src={Logo} alt="logo" /> */}
+      <div>{useNickname}'s World!</div>{" "}
+    </div>
+  );
 };
 
 export default Title;
