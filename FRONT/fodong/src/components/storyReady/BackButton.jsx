f537238eb6c39cbdbe9d53d244c8d3837bb07e0d
전재완 <wodhksdl7893@naver.com>
2024-02-12 12:59:55 +0900

    [feat] : 동화 구연 캐릭터 선택창
    Former-commit-id: 7e0d88e9d260ba434fdbcdd20ba13fd0a24ad0b0
    Former-commit-id: 9d43d1700f143018b5db7b40518092e1da1e2971

--- FRONT/fodong/src/components/storyReady/BackButton.jsx
+++ FRONT/fodong/src/components/storyReady/BackButton.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const BackButton = () => {
+const BackButton = ({ style }) => {
   return (
-    <div>
+    <div style={style}>
       <Link to="/main">
-        <img src={require(`./img/back.png`)} alt="startbutton" style={{marginTop: '10', maxWidth: '100px', maxHeight: '100px'}} />
+        <img src={require(`./img/back1.png`)} alt="startbutton" style={{ maxWidth: '100px', maxHeight: '100px'}} />
       </Link>
+      <p style={{ textAlign: "center", fontWeight: "bold" }}>
+          <h1>뒤로가기~</h1>
+        </p>
     </div>
   );
 };
--- FRONT/fodong/src/components/storyReady/Background.jsx
+++ FRONT/fodong/src/components/storyReady/Background.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import backgroundImage from "./img/story1.png";
 import ant from "./img/1.jpg";
 import grasshopper from "./img/grasshoper2.png";
-
+import BackButton from "./BackButton";
+import StartButton from "./StartButton";
 
 const Background = () => {
   console.log(1232);
@@ -18,10 +19,74 @@ const Background = () => {
           justifyContent: "center",
           alignItems: "center",
           opacity: "0.6",
+          position: "relative",
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            height: "100%",
+            borderRight: "4px dashed red",
+            zIndex: 1,
+          }}
+        ></div>
+      </div>
+          <BackButton
+            style={{
+              position: "absolute",
+              bottom: "20px",
+              left: "20px",
+              zIndex: 4,
+            }}
+          />
+          <StartButton
+            style={{
+              position: "absolute",
+              bottom: "20px",
+              right: "20px",
+              zIndex: 1,
+            }}
+          />
+      <img
+        src={ant}
+        style={{
+          width: "50vw",
+          maxWidth: "400px",
+          height: "auto",
+          position: "absolute",
+          left: "23%",
+          top: "50%",
+          transform: "translate(-50%, -50%)",
+          zIndex: 2,
+        }}
+        alt="Ant"
+      />
+      <img
+        src={grasshopper}
+        style={{
+          width: "50vw",
+          maxWidth: "400px",
+          height: "auto",
+          position: "absolute",
+          left: "75%",
+          top: "47%",
+          transform: "translate(-50%, -50%)",
           zIndex: 0,
         }}
-      ></div>
-       
+        alt="Ant"
+      />
+      <p
+        style={{
+          position: "absolute",
+          top: "6%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          fontSize: "7vw",
+          zIndex: 3,
+        }}
+      >
+        개미와 베짱이
+      </p>
     </div>
   );
 };
