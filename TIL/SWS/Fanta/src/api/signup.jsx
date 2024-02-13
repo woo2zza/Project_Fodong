49201efb59dfde61d0331c4ad43c20720a126400
kjm <willom2c@naver.com>
2024-02-13 09:40:51 +0900

    [etc] : 삭제
    Former-commit-id: 972748cdc47870a5d07824ac81c8cfb2a0c4fc17
    Former-commit-id: 3a46888b1585552faee37be20535221ce248ef8e

--- TIL/SWS/Fanta/src/api/signup.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-export const signup = async (id, pwd, email) => {
-  const result = await axios.post("", {
-    id,
-    pwd,
-    email,
-  });
-  return result.data;
-};
