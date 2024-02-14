2ddd2cc3775d57d72e9a528e97162eecb19325f7
심우석 <dntjr5741@naver.com>
2024-02-14 21:54:44 +0900

    [feat] : storyready 구조화 및 css
    Former-commit-id: 4371c7da0c8b436e4b3367424fd13621d3e95cff
    Former-commit-id: 6434843522ab0ed7b4a3a32d52ed0000a4793a04

--- FRONT/fodong/src/components/multi/StoryRoom.jsx
+++ FRONT/fodong/src/components/multi/StoryRoom.jsx
@@ -306,10 +306,8 @@ const StoryRoom = ({
     <div className="Room-container">
       {!playState ? (
         <div id="join" className="storyRoomWrapper">
-          <div id="join-dialog" className="storyRoomDialog">
+          <div id="join-dialog">
             <Webcam className="web-container" />
-            <h1>동화 만들기~</h1>
-
             {/* <form className="form-group" onSubmit={joinSession}> */}
             {/* <form className="form-group" onSubmit={sendStartRequest}> */}
             <form
