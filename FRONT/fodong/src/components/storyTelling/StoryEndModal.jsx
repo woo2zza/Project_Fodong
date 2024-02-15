c694daeae5f7fd92e369295679219f1b816a6aa2
전재완 <wodhksdl7893@naver.com>
2024-02-15 11:36:48 +0900

    [feat] : 동화구연 대사 캐릭터
    Former-commit-id: 6dbb0cc676fd06cbd9df22e1712dd2569743e78b
    Former-commit-id: 0b8c35f124ecae6622e793b4c433d89837a18e92

--- FRONT/fodong/src/components/storyTelling/StoryEndModal.jsx
+++ FRONT/fodong/src/components/storyTelling/StoryEndModal.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
 import "./EndModal.css"
-import end from "./end.webp"
+import end from "./img/end.webp"
 
 
 const StoryEndModal = ({onClose, onBack, onRecording}) => {
